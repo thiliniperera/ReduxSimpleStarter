f95e66f3f88544c7dbea818ebe55bb562a48862e
thiliniperera <Perera.Thilini@corp.sysco.com>
2018-01-14 17:35:43 +0530

    Change the components state when the input is changed

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from "react";
 
 class SearchBar extends Component{
 
@@ -9,7 +9,13 @@ class SearchBar extends Component{
     }
 
     render(){
-        return <input onChange = { (event) => { console.log(event.target.value); } } />;
+        return (
+            <div>
+                <input onChange={ (event) => {
+                    this.setState({term: event.target.value})
+                } }/>
+            </div>
+        );
     }
 }
 
