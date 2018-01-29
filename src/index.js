56f66d60b027f1393a056beb3c44c3edff27e5a9
thiliniperera <Perera.Thilini@corp.sysco.com>
2018-01-29 07:14:08 +0530

    Adding youtube search to App component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyD4svzC5bKAHwLViCAbQeQweECsVfROLtU';
 
-const App = () => {
-  return (
+
+
+class App extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = { videos: []};
+
+        YTSearch({key:API_KEY, term: 'Ed Sheeran'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render(){
+        return (
       <div>
         <SearchBar/>
-      </div>);
+      </div>
+  )};
 }
 
 ReactDOM.render(<App />,document.querySelector('.container'));
